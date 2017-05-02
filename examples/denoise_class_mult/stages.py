f1fbf6bd356fbf64dd5a7598319d0c962c71a54a
Becktor <JonathanBecktor@gmail.com>
2017-05-02 16:24:33 +0200

    denoise changes

--- examples/denoise_class_mult/stages.py
+++ examples/denoise_class_mult/stages.py
@@ -1,18 +1,24 @@
 import tensorflow as tf
 import numpy as np
 import os
+import io
 from datetime import datetime
 import matplotlib
 matplotlib.use('Agg')
 
 import matplotlib.pyplot as plt
 
-from flowfairy.core.stage import register, Stage
+from flowfairy.core.stage import register, Stage, stage
 from flowfairy.conf import settings
 
-log_dir = os.path.join(settings.LOG_DIR, settings.LOGNAME)
 
-@register(500)
+def get_log_dir():
+    return os.path.join(settings.LOG_DIR, settings.LOGNAME)
+
+def norm(tensor):
+    return tf.div((tensor - tf.reduce_min(tensor)), (tf.reduce_max(tensor) - tf.reduce_min(tensor)))
+
+@register(100)
 class SummaryStage(Stage):
     def fig2rgb_array(self, expand=True):
         self.figure.canvas.draw()
@@ -21,7 +27,6 @@ class SummaryStage(Stage):
         shape = (nrows, ncols, 3) if not expand else (1, nrows, ncols, 3)
         return np.fromstring(buf, dtype=np.uint8).reshape(shape)
 
-
     def reset_fig(self):
         self.figure = plt.figure(num=0, figsize=(6,4), dpi=300)
         self.figure.clf()
@@ -29,38 +34,46 @@ class SummaryStage(Stage):
     def before(self, sess, net):
         tf.summary.scalar('acc', net.accuracy)
         tf.summary.scalar('cost', net.cost)
+
         self.pred = net.pred
         self.x = net.x
         self.y = net.y
-        #save sound
-        arg = tf.argmax(self.pred,2)
-        tf.summary.audio('x', tf.cast(self.y, tf.float32), settings.SAMPLERATE)
-        tf.summary.audio('pred',tf.cast(arg, tf.float32), settings.SAMPLERATE)
-        self.chunk=net.chunk
-        #save fig
+        self.chunk = net.chunk
+
+        arg = tf.argmax(self.pred, 2)
+        tf.summary.audio('x', norm(tf.cast(self.y, tf.float32)), settings.SAMPLERATE)
+        tf.summary.audio('pred',norm(tf.cast(arg, tf.float32)), settings.SAMPLERATE)
+
         self.reset_fig()
         img = self.fig2rgb_array()
-        self.image = tf.Variable(np.zeros(img.shape, dtype=np.uint8))
+
+        self.image_in = tf.placeholder(np.uint8, shape=img.shape)
+        self.image = tf.Variable(np.zeros(img.shape, dtype=np.uint8), trainable=False, name='graph_image')
+        self.image_assign = self.image.assign(self.image_in)
+
         tf.summary.image('graph', self.image)
-        #merge tf summaries
+
         self.merged = tf.summary.merge_all()
-        self.writer = tf.summary.FileWriter(os.path.join(settings.LOG_DIR, str(datetime.now())), sess.graph)
+        self.writer = tf.summary.FileWriter(get_log_dir(), sess.graph)
 
     def plot(self, sess):
         self.reset_fig()
 
-        res, x, y, c = sess.run([self.pred, self.x, self.y, self.chunk])
+        res, x, y, c= sess.run([ self.pred, self.x, self.y, self.chunk])
         res = np.argmax(res, 2)
-        start = c[0]-50
-        end = (start+settings.CHUNK+100)
+
+        start = c[0] - settings.CHUNK
+        end = start+settings.CHUNK*3
+
         plt.subplot('111').plot(res[0,start:end],'r')
         plt.subplot('111').plot(y[0,start:end],'b', alpha=0.5)
         plt.subplot('111').plot(x[0,start:end],'g', alpha=0.5)
 
 
+
     def draw_img(self, sess):
         self.plot(sess)
-        sess.run(self.image.assign(self.fig2rgb_array()))
+        sess.run(self.image_assign, feed_dict={self.image_in: self.fig2rgb_array()})
 
     def run(self, sess, i):
         self.draw_img(sess)
@@ -80,10 +93,4 @@ class TrainingStage(Stage):
     def run(self, sess, i):
         sess.run(self.optimizer)
 
-#@register(1000)
-#class SavingStage(Stage):
-#    def before(self, sess, net):
-#        self.saver = tf.train.Saver()
-#
-#    def run(self, sess, i):
-#        self.saver.save(sess, log_dir, global_step=i)
+
