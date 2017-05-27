c3fafdb58bce747ae0fca33bd14e960d4af47ba6
Becktor <JonathanBecktor@gmail.com>
2017-05-27 15:33:44 +0200

    Jbb sinefix

--- examples/sine_fix/midembglunet.py
+++ examples/sine_fix/midembglunet.py
@@ -1,3 +1,4 @@
+import ops
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 from flowfairy.conf import settings
@@ -20,41 +21,46 @@ def broadcast(l, emb):
 
 # Create model
 def conv_net(x, cls, dropout, is_training=False):
-    xs = tf.reshape(x, shape=[-1, 11024, 1, 1])
+    xs = tf.reshape(x, shape=[-1, sr, 1, 1])
 
-    conv1 = GLU(xs, 4, [256, 1], scope='conv1_1')
+    conv1 = GLU(xs, 4, [256, 1],  scope='conv1_1',
+                normalizer_fn=slim.batch_norm,
+                normalizer_params={'is_training': is_training,
+                                   'decay': 0.9})
+    print('conv1: ', conv1)
     conv1 = GLU(conv1, 4, [256, 1], scope='conv1_2')
-    pool1 = slim.max_pool2d(conv1, [2,1])
-    print('conv1: ', pool1)
+    #pool1 = slim.max_pool2d(conv1, [2,1])
+    print('conv1: ', conv1)
 
 
     #convblock 2
     conv2 = GLU(conv1, 8, [128, 1], scope='conv2_1')
+    print('conv2: ', conv2)
     conv2 = GLU(conv2, 8, [128, 1], scope='conv2_2')
-    #pool2 = slim.max_pool2d(conv2, [2,1])
+    print('conv2: ', conv2)
+    pool2 = slim.max_pool2d(conv2, [2,1])
     print('conv2: ', pool2)
 
     #convblock 3
     with tf.variable_scope('embedding'):
         emb1 = embedding(cls, embedding_size, num_classes)
-        embedded = broadcast(conv2, emb1)
+        embedded = broadcast(pool2, emb1)
         print('embedded:', embedded)
 
     conv3 = GLU(embedded, 16, [128, 1], scope='conv3_1')
-    conv3 = GLU(conv3, 32, [128, 1], scope='conv3_2')
+    conv3 = GLU(conv3, 16, [128, 1], scope='conv3_2')
     print('conv3: ', conv3)
 
     with tf.name_scope('d2s1'):
-        conv4 = tf.depth_to_space(conv3, 2) #upconv
+        conv4 = tf.depth_to_space(conv3, 4) #upconv
+        conv4 = tf.reshape(conv4, shape=[-1, sr, 1, 8])
         print('d2sp: ', conv4)
-        conv4 = tf.reshape(conv4, shape=[-1, sr, 1, 16])
 
-    conv4 = GLU(conv4, 32, [128, 1], scope='conv4_1')
+    conv4 = GLU(conv4, 16, [128, 1], scope='conv4_1')
     with tf.name_scope('concat'):
         conv4 = tf.concat([conv4, conv1], 3) # <- unet like concat first with last
-    conv4 = GLU(conv4, 64, [128, 1], scope='conv4_2')
+    conv4 = GLU(conv4, 32, [128, 1], scope='conv4_2')
     print('conv4: ', conv4)
-
 
     conv5 = GLU(conv4, discrete_class, [2,1], scope='conv5')
     print('conv5: ', conv5)
@@ -69,7 +75,7 @@ class Net:
     def __init__(self):
         pass
 
-    def feedforward(self, x, y, frqid, is_training=False):
+    def feedforward(self, x, y, frqid, frqid2, is_training=False):
         pred = conv_net(x, frqid, None, is_training)
 
         target_output = tf.reshape(y,[-1])
@@ -77,9 +83,9 @@ class Net:
 
         # Define loss and optimizer
         with tf.name_scope('cost'):
-            sparse = tf.nn.sparse_softmax_cross_entropy_with_logits(logits = prediction,
-                                                                    labels = target_output)
-            cost = tf.reduce_mean(sparse)
+            cost = tf.losses.sparse_softmax_cross_entropy(logits = prediction,
+                                                          labels = target_output,
+                                                          scope='xentropy')
 
         correct_pred = tf.equal(tf.argmax(pred, 2), y)
         with tf.name_scope('accuracy'):
@@ -93,10 +99,7 @@ class Net:
             self.train_y = kwargs['y']
 
             self.train_pred, self.train_cost, self.train_acc = self.feedforward(is_training=True, **kwargs)
-            self.optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(self.train_cost)
-        #gradients, variables = zip(*optimizer.compute_gradients(self.train_cost))
-        #gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
-        #self.optimizer = optimizer.apply_gradients(zip(gradients, variables))
+            self.optimizer = ops.train()
 
     def validation(self, **kwargs):
         with tf.name_scope('val'):
@@ -104,6 +107,8 @@ class Net:
             self.val_y = kwargs['y']
 
             self.val_pred, self.val_cost, self.val_acc = self.feedforward(**kwargs)
+            self.val_pred = tf.Print(self.val_pred, [kwargs['frqid'], kwargs['frqid2']], message='frqids: ')
+
 
 
     def begin(self, session):
--- examples/sine_fix/settings.py
+++ examples/sine_fix/settings.py
@@ -2,7 +2,8 @@ import os
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
-NET = 'dilated'
+NET = 'midembglunet'
+
 
 FEATURES = [
     'feature.SineGen',
@@ -21,7 +22,8 @@ FREQUENCY_START = 200
 FREQUENCY_LIMIT = (FREQUENCY_START, FREQUENCY_START + CLASS_COUNT*2)
 EMBEDDING_SIZE = 2
 DISCRETE_CLASS = 256
-MAX_AMP = 3
+MAX_AMP = 2
+
 
 
 BATCH_SIZE = 32
