403d01a62e73afdd5307f2383bda6b2405afbc9a
Becktor <JonathanBecktor@gmail.com>
2017-05-31 14:26:45 +0200

    jbb binaural

--- examples/sine_fix/settings.py
+++ examples/sine_fix/settings.py
@@ -2,7 +2,7 @@ import os
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 
-NET = 'net2'
+NET = 'jobeNet'
 
 FEATURES = [
     #'feature.SineGen',
@@ -37,6 +37,6 @@ LOG_DIR = os.path.join(base_dir, "logs")
 
 DATA = {
     'train': (),
-    'validation': (),
+    # 'validation': (),
     'test': ()
 }
